2775bd3403af2f23e2971dbf7d934fec8d4ccd65
Nelson Ding <nding17@outlook.com>
2020-06-03 21:03:55 -0400

    formatting for the setup file

--- setup.py
+++ setup.py
@@ -4,20 +4,24 @@ setup(
   name = 'jupyslides',         
   packages = ['jupyslides'],
   version = '0.1',
-  license='MIT',
+  license = 'MIT',
   description = 'A jupyter notebook widget that allows you to create an embedded ppt slides',
   author = 'Naili Ding',
   author_email = 'nding17@outlook.com',
   url = 'https://github.com/nding17/jupyslides',
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['jupyter notebook', 'embedded ppt', 'embedded slides'],   # Keywords that define your package best
-  install_requires=[
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords = [
+    'jupyter notebook', 
+    'embedded ppt', 
+    'embedded slides'
+  ],
+  install_requires = [
     'ipywidgets',
     'os',
     'natsort',
     'IPython',
   ],
-  classifiers=[
+  classifiers = [
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
